12172d0db931e11a70c0f2928eef58e1be2c0987
Mateus Barboza <mateusbarboza1898@gmail.com>
2025-09-23 16:16:59 +0000

    xadrez23/09

--- xadrez.c
+++ xadrez.c
@@ -2,17 +2,21 @@
 
 int main() {
 
-    
-    int i;
-    int casasTorre = 5;
-    int casasBispo = 5;
-    int casasRainha = 8;
 
+    int i, j; 
+    int casasTorre  = 5;
+    int casasBispo  = 5;
+    int casasRainha = 8;
+    int casasBaixo  = 2;
+    int casasEsq    = 1;
+
+    
     printf("Movimento da Torre (FOR):\n");
     for (i = 1; i <= casasTorre; i++) {
         printf("Direita - Casa %d\n", i);
     }
 
+    
     printf("\nMovimento do Bispo (WHILE):\n");
     i = 1;
     while (i <= casasBispo) {
@@ -20,6 +24,7 @@ int main() {
         i++;
     }
 
+    
     printf("\nMovimento da Rainha (DO-WHILE):\n");
     i = 1;
     do {
@@ -27,6 +32,20 @@ int main() {
         i++;
     } while (i <= casasRainha);
 
+   
+    printf("\nMovimento do Cavalo (FOR + WHILE):\n");
+
+   
+    for (i = 1; i <= casasBaixo; i++) {
+        printf("Baixo - Casa %d\n", i);
+    }
+
+   
+    j = 1;
+    while (j <= casasEsq) {
+        printf("Esquerda - Casa %d\n", j);
+        j++;
+    }
+
     return 0;
 }
-
