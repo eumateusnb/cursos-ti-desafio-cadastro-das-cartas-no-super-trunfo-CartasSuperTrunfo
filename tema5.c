1fa4227d34d830a2e4a713b5798a795bdd8092d3
Mateus Barboza <mateusbarboza1898@gmail.com>
2025-10-01 12:44:11 +0000

    tema5

--- tema5.c
+++ tema5.c
@@ -15,12 +15,10 @@ int main() {
     }
 
     int navios[4][3] = {
-        // Navios Horizontais/Verticais (Normais)
-        {1, 1, 0}, // Navio 1: Horizontal, começa em (1, 1)
-        {5, 8, 1}, // Navio 2: Vertical, começa em (5, 8)
-        
-        {7, 0, 2}, // Navio 3: Diagonal Principal (i+, j+), começa em (7, 0)
-        {0, 9, 3}  // Navio 4: Diagonal Secundária (i+, j-), começa em (0, 9)
+        {1, 1, 0}, // Navio 1: Horizontal
+        {5, 8, 1}, // Navio 2: Vertical
+        {7, 0, 2}, // Navio 3: Diagonal Principal
+        {0, 9, 3}  // Navio 4: Diagonal Secundária
     };
     
     int navios_posicionados = 0;
@@ -33,7 +31,6 @@ int main() {
         int direcao = navios[k][2];
         
         if (validar_posicao(tabuleiro, linha, coluna, direcao, TAMANHO_NAVIO)) {
-            // 3. Posicionamento do Navio
             posicionar_navio(tabuleiro, linha, coluna, direcao, TAMANHO_NAVIO);
             navios_posicionados++;
             printf("Navio %d posicionado com sucesso em (%d, %d), direção %d.\n", k + 1, linha, coluna, direcao);
@@ -45,62 +42,45 @@ int main() {
     printf("\nTotal de navios posicionados: %d/4\n", navios_posicionados);
     
     printf("\n--- Tabuleiro Final do Batalha Naval ---\n");
-    (tabuleiro);
+    exibir_tabuleiro(tabuleiro);
     printf("Legenda: %d = Água, %d = Navio\n", AGUA, NAVIO);
     
+    return 0;
 }
-    (int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO], int linha_inicial, int coluna_inicial, int direcao, int tamanho) {
+
+bool validar_posicao(int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO], int linha_inicial, int coluna_inicial, int direcao, int tamanho) {
     for (int i = 0; i < tamanho; i++) {
         int l = linha_inicial;
         int c = coluna_inicial;
 
         switch (direcao) {
-            case 0: // Horizontal (coluna++)
-                c += i;
-                break;
-            case 1: // Vertical (linha++)
-                l += i;
-                break;
-            case 2: // Diagonal Principal (linha++, coluna++)
-                l += i;
-                c += i;
-                break;
-            case 3: // Diagonal Secundária (linha++, coluna--)
-                l += i;
-                c -= i;
-                break;
-            default:
+            case 0: c += i; break;             // Horizontal
+            case 1: l += i; break;             // Vertical
+            case 2: l += i; c += i; break;     // Diagonal Principal
+            case 3: l += i; c -= i; break;     // Diagonal Secundária
         }
 
         if (l < 0 || l >= TAMANHO_TABULEIRO || c < 0 || c >= TAMANHO_TABULEIRO) {
+            return false;
         }
 
         if (tabuleiro[l][c] != AGUA) {
+            return false;
         }
     }
+    return true;
 }
 
 void posicionar_navio(int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO], int linha_inicial, int coluna_inicial, int direcao, int tamanho) {
-
     for (int i = 0; i < tamanho; i++) {
         int l = linha_inicial;
         int c = coluna_inicial;
 
         switch (direcao) {
-            case 0: // Horizontal
-                c += i;
-                break;
-            case 1: // Vertical
-                l += i;
-                break;
-            case 2: // Diagonal Principal (i+, j+)
-                l += i;
-                c += i;
-                break;
-            case 3: // Diagonal Secundária (i+, j-)
-                l += i;
-                c -= i;
-                break;
+            case 0: c += i; break;
+            case 1: l += i; break;
+            case 2: l += i; c += i; break;
+            case 3: l += i; c -= i; break;
         }
     
         tabuleiro[l][c] = NAVIO;
@@ -108,22 +88,21 @@ void posicionar_navio(int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO], int l
 }
 
 void exibir_tabuleiro(int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO]) {
-    printf("   "); // Espaço para alinhar com o índice da linha
+    printf("   ");
     for (int j = 0; j < TAMANHO_TABULEIRO; j++) {
-        printf("%3d", j); // 3 espaços para cada índice de coluna
+        printf("%3d", j);
     }
     printf("\n");
     
-    printf("  +-------------------------------\n");
-
+    printf("  +---------------------------------------------------\n");
 
     for (int i = 0; i < TAMANHO_TABULEIRO; i++) {
-        printf("%d |", i); // Imprime o índice da linha
+        printf("%2d|", i);
         for (int j = 0; j < TAMANHO_TABULEIRO; j++) {
             printf("%3d", tabuleiro[i][j]); 
         }
-        printf("\n"); // Nova linha ao final de cada linha do tabuleiro
+        printf("\n");
     }
-return 0;
 }
+
 
